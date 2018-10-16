2b4e1f0d24c04e90ba93ad473c4a4aaf2f3c66aa
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-10-16 20:26:44 -0300

    Add files via upload

---
+++ Prova1/Prova1/IMC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova1
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+        //Construtores
+        public IMC() { }
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        //Método que calcula o IMC
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+            
+        }
+
+        public double Classificar(double imc)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ Prova1/Prova1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Prova1
+{
+    class Program
+    {
+        //Exercicio 1
+        static int Maior(int A, int B)
+        {
+            if (A > B)
+                return A;
+            else
+                return B;
+        }
+        //SEGUNDA FORMA
+        static int Maior2(int A, int B)
+        {
+            return (A > B) ? A : B;
+            // Operador ternario - : se sim retorna o primeiro senao retorna o segundo
+        }
+        //Exercicio 3
+        static bool Par(int numero)
+        {
+            return (numero % 2 == 0) ? true : false;
+        }
+
+        //Para escrever se é par ou impar
+        static string Par1(int numero)
+        {
+            return (numero % 2 == 0) ? "O numero é par" : "O numero é impar";
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Maior2(1, 2));
+
+            //Console.WriteLine(Par1(2));
+            Console.ReadKey();
+        }
+
+
+    }
+}
